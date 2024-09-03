6a7c2380d190123ed1d75bb4a75a0f0bcacecb41
Jesse Nickles <jesse@littlebizzy.com>
2024-09-03 16:40:06 +0700

    Update disable-empty-trash.php

--- disable-empty-trash.php
+++ disable-empty-trash.php
@@ -3,7 +3,7 @@
 Plugin Name: Disable Empty Trash
 Plugin URI: https://www.littlebizzy.com/plugins/disable-empty-trash
 Description: Stops WordPress emptying trash
-Version: 1.1.0
+Version: 2.0.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPL3
@@ -13,76 +13,21 @@ Primary Branch: master
 Prefix: DETTRS
 */
 
-// disable wordpress.org updates
-add_filter(
-	'gu_override_dot_org',
-	function ( $overrides ) {
-		return array_merge(
-			$overrides,
-			array( 'disable-empty-trash/disable-empty-trash.php' )
-		);
-	}
-);
-
+// Disable WordPress.org updates for this plugin
+add_filter('gu_override_dot_org', function ($overrides) {
+    $overrides['disable-empty-trash/disable-empty-trash.php'] = true;
+    return $overrides;
+});
 
 /**
- * Define main plugin class
+ * Disable automatic trash emptying.
  */
-class LB_Disable_Empty_Trash {
-
-	/**
-	 * A reference to an instance of this class.
-	 *
-	 * @since 1.0.0
-	 * @var   object
-	 */
-	private static $instance = null;
-
-	/**
-	 * Initalize plugin actions
-	 *
-	 * @return void
-	 */
-	public function init() {
-		remove_action( 'wp_scheduled_delete', 'wp_scheduled_delete' );
-	}
-
-	/**
-	 * Returns plugin base file
-	 *
-	 * @return string
-	 */
-	public static function file() {
-		return __FILE__;
-	}
-
-	/**
-	 * Returns the instance.
-	 *
-	 * @since  1.0.0
-	 * @return object
-	 */
-	public static function get_instance() {
-
-		// If the single instance hasn't been set, set it now.
-		if ( null == self::$instance ) {
-			self::$instance = new self;
-		}
-
-		return self::$instance;
-	}
+function disable_empty_trash() {
+    // Remove the scheduled action for emptying trash
+    remove_action('wp_scheduled_delete', 'wp_scheduled_delete');
 }
 
-/**
- * Returns instance of LB_Disable_Empty_Trash class
- *
- * @return object
- */
-function lb_disable_empty_trash() {
-	return LB_Disable_Empty_Trash::get_instance();
-}
+// Hook the function to 'init' with high priority
+add_action('init', 'disable_empty_trash', -999);
 
-/**
- * Initalize plugin instance very early on 'init' hook
- */
-add_action( 'init', array( lb_disable_empty_trash(), 'init' ), -999 );
+// Ref: ChatGPT
