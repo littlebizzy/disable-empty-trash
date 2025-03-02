a32462303387a194f55bde93d62741b396f7392e
Jesse Nickles <jesse@littlebizzy.com>
2025-03-02 14:51:18 +0700

    Update disable-empty-trash.php

--- disable-empty-trash.php
+++ disable-empty-trash.php
@@ -3,14 +3,17 @@
 Plugin Name: Disable Empty Trash
 Plugin URI: https://www.littlebizzy.com/plugins/disable-empty-trash
 Description: Stops WordPress emptying trash
-Version: 2.0.2
+Version: 2.0.3
 Requires PHP: 7.0
+Tested up to: 6.7
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPL3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Update URI: false
 GitHub Plugin URI: littlebizzy/disable-empty-trash
 Primary Branch: master
+Text Domain: disable-empty-trash
 */
 
 // prevent direct access
@@ -18,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-// disable wordpress.org updates for this plugin
+// override wordpress.org with git updater
 add_filter( 'gu_override_dot_org', function( $overrides ) {
     $overrides[] = 'disable-empty-trash/disable-empty-trash.php';
     return $overrides;
@@ -26,11 +29,11 @@ add_filter( 'gu_override_dot_org', function( $overrides ) {
 
 // disable automatic trash emptying
 function disable_empty_trash() {
-    // Remove the scheduled action for emptying trash
-    remove_action('wp_scheduled_delete', 'wp_scheduled_delete');
+    // remove scheduled trash deletion
+    remove_action( 'wp_scheduled_delete', 'wp_scheduled_delete' );
 }
 
-// Hook the function to 'init' with high priority
-add_action('init', 'disable_empty_trash', -999);
+// hook to init with high priority
+add_action( 'init', 'disable_empty_trash', -999 );
 
 // Ref: ChatGPT
